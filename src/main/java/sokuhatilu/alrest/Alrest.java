f5f47cb19a7922d945facd185dbf44b5fd6263cf
Sokuhatiku <sokuhatiku@gmail.com>
2022-09-03 04:04:15 +0900

    modの初期設定

---
+++ src/main/java/sokuhatilu/alrest/Alrest.java
@@ -0,0 +1,10 @@
+package sokuhatilu.alrest;
+
+import com.mojang.logging.LogUtils;
+import net.minecraftforge.fml.common.Mod;
+import org.slf4j.Logger;
+
+@Mod("alrest")
+public class Alrest {
+    private static final Logger LOGGER = LogUtils.getLogger();
+}
