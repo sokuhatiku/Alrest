15871995d7581d50a7ea0601430f5f6efb39fb3e
Sokuhatiku <sokuhatiku@gmail.com>
2023-11-11 19:28:03 +0900

    以前作業していたものを今後作業しやすくなるように修正

---
+++ src/main/java/net/sokuhatilu/alrest/Alrest.java
@@ -0,0 +1,23 @@
+package net.sokuhatilu.alrest;
+
+import com.mojang.logging.LogUtils;
+import net.minecraft.world.level.block.Blocks;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import org.slf4j.Logger;
+
+@Mod(Alrest.MOD_ID)
+public class Alrest {
+    public static final String MOD_ID = "alrest";
+    private static final Logger LOGGER = LogUtils.getLogger();
+
+    public Alrest(){
+        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::setup);
+    }
+
+    private void setup(final FMLCommonSetupEvent event) {
+        LOGGER.info("HELLO FROM PREINIT");
+        LOGGER.info("DIRT BLOCK >> {}", Blocks.DIRT.getRegistryName());
+    }
+}
